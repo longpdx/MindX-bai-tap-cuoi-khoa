05336092f405da5013cf21cc53da9cd4920efa36
Long Truong MindX <longtruong2790@gmail.com>
2023-04-03 21:55:22 -0700

    update animation scrolling

--- animation.js
+++ animation.js
@@ -1,4 +1,16 @@
-  //Animation
+  // Animation Text + Ship
+  const text = document.getElementById('text');
+  const ship = document.getElementById('ship');
+
+  window.addEventListener('scroll', function(){
+    const value = window.scrollY;
+
+    text.style.marginTop = value * 1.5 + 'px';
+    ship.style.left = value * 1.5 + 'px';
+  });
+  // Animation Text + Ship End
+  
+  //Animation Wave
   const wave1 = document.getElementById('wave1');
   const wave2 = document.getElementById('wave2');
   const wave3 = document.getElementById('wave3');
@@ -11,6 +23,5 @@
     wave2.style.backgroundPositionX = 200 + value + -4 + 'px';
     wave3.style.backgroundPositionX = 100 + value + 2 + 'px';
     wave4.style.backgroundPositionX = 400 + value + -2 + 'px';
-  }
-  )
-  // Animation End  
+  });
+  // Animation Wave End  
