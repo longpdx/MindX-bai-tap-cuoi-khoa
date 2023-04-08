c25a06eaf713a2c53226a4e1a530dcd95195ceee
Long Truong MindX <longtruong2790@gmail.com>
2023-04-08 14:10:00 -0700

    videos update

--- scripts/popup-video.js
+++ scripts/popup-video.js
@@ -1,15 +1,19 @@
-var array_item = [
-  [1, '1.mp4'],
-  [2, '2.mp4'],
-  [3, '3.mp4'],
-  [4, '4.mp4'],
-  [5, '5.mp4'],
-  [6, '6.mp4'],
-  [7, '7.mp4'],
-  [8, '8.mp4'],
-  [9, '9.mp4'],
-  [10, '10.mp4'],
-]
+// var array_item = [
+//   [1, '1.mp4'],
+//   [2, '2.mp4'],
+//   [3, '3.mp4'],
+//   [4, '4.mp4'],
+//   [5, '5.mp4'],
+//   [6, '6.mp4'],
+//   [7, '7.mp4'],
+//   [8, '8.mp4'],
+//   [9, '9.mp4'],
+//   [10, '10.mp4'],
+// ]
+var array_item = [];
+for (var i = 1; i <= 47; i++) {
+  array_item.push([i, i + '.mp4']);
+}
 
   function open_popup(value){
     document.getElementById('modal-popup').style.display='block';
